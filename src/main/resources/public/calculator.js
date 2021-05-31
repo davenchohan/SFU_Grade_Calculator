b883ca6061b2cde5d6eafecb3178e30ab2b39730
Daven <dca120@sfu.ca>
2021-05-30 17:09:49 -0700

    fixed negative options

--- src/main/resources/public/calculator.js
+++ src/main/resources/public/calculator.js
@@ -2,7 +2,7 @@
 function test1(){
     numGrades = 4;
     percent1 = document.getElementById("mark1").value/document.getElementById("total1").value;
-    if(isNaN(percent1)||document.getElementById("total1").value<=0){
+    if(isNaN(percent1)||document.getElementById("total1").value<=0||percent1<0){
         document.getElementById("displayP1").innerHTML = null;
         percent1=0;
         numGrades--; 
@@ -11,7 +11,7 @@ function test1(){
         document.getElementById("displayP1").innerHTML = percent1.toFixed(2)*100 + "%";
     } 
     percent2 = document.getElementById("mark2").value/document.getElementById("total2").value;
-    if(isNaN(percent2)||document.getElementById("total2").value<=0){
+    if(isNaN(percent2)||document.getElementById("total2").value<=0||percent2<0){
         document.getElementById("displayP2").innerHTML = null;
         percent2=0;
         numGrades--;  
@@ -20,7 +20,7 @@ function test1(){
         document.getElementById("displayP2").innerHTML = percent2.toFixed(2)*100 + "%";
     }  
     percent3 = document.getElementById("mark3").value/document.getElementById("total3").value;
-    if(isNaN(percent3)||document.getElementById("total3").value<=0){
+    if(isNaN(percent3)||document.getElementById("total3").value<=0||percent3<0){
         document.getElementById("displayP3").innerHTML = null;
         percent3=0;
         numGrades--;  
@@ -29,7 +29,7 @@ function test1(){
         document.getElementById("displayP3").innerHTML = percent3.toFixed(2)*100 + "%";
     } 
     percent4 = document.getElementById("mark4").value/document.getElementById("total4").value;
-    if(isNaN(percent4)||document.getElementById("total4").value<=0){
+    if(isNaN(percent4)||document.getElementById("total4").value<=0||percent4<0){
         document.getElementById("displayP4").innerHTML = null;
         percent4=0;
         numGrades--;  
